88ea2c4ae5e4f355dd0432ec04bbe87887390f22
duckupus <75720972+duckupus@users.noreply.github.com>
2022-09-28 15:56:21 +0800

    first "working" commit

--- src/main.c
+++ src/main.c
@@ -1,5 +1,93 @@
 #include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include "permutate.h"
 
-int main() {
+static void help() {
+	puts("Usage:");
+	puts("hasher [OPTIONS]");
+	puts("-h\t--help prints help message");
+	puts("-H\t--hash [md5 hash] e.g. -H dd76adf401d8ed0cc5485230dc83758b");
+	puts("-m\t--min [min value(inclusive)] e.g. -m 5");
+	puts("-M\t--max [max value(inclusive)] e.g. -M 10");
+	puts("-a\t--alph sets min max value to A-z");
+	puts("-A\t--anum sets min max value to all printable ascii values"); //according to ascii chart
+}
+
+static struct option long_options[] = {
+	{"help", no_argument, NULL, 'h'},
+	{"hash", required_argument, NULL, 'H'},
+	{"min", required_argument, NULL, 'm'},
+	{"max", required_argument, NULL, 'M'},
+	{"alph", no_argument, NULL, 'a'},
+	{"anum", no_argument, NULL, 'A'}
+};
+
+int main(int argc, char **argv) {
+	int i;
+	int hash, min, max;
+	hash = 0;
+	min = 0;
+	max = 0;
+	br_md5 ctx;
+	long srlen;
+	char *val;
+	while((i = getopt_long(argc, argv, "hH:m:M:aA", long_options, NULL)) != -1)  {
+		switch(i) {
+			case 'h':
+				help();
+				return 0;
+				break;
+			case 'H':
+				srlen = strlen(optarg);
+				ctx.hash = malloc(srlen*sizeof(char));
+				ctx.hash[srlen] = 0x00;
+				strncpy(ctx.hash, optarg, srlen);
+				hash = 1;
+				break;
+			case 'm':
+				ctx.min = atoi(optarg);
+				min = 1;
+				break;
+			case 'M':
+				ctx.max = atoi(optarg);
+				max = 1;
+				break;
+			case 'a':
+				ctx.min = 'A';
+				min = 1;
+				ctx.max = 'z';
+				max = 1;
+				break;
+			case 'A':
+				ctx.min = ' ';
+				min = 1;
+				ctx.max = '~';
+				max = 1;
+				break;
+		}
+	}
+	ctx.len = 0;
+	if(!hash) {
+		help();
+		exit(-1);
+	}
+	if(!min) ctx.min = 0x00;
+	if(!max) ctx.max = 0xff;
+	if(ctx.min >= ctx.max) {
+		fprintf(stderr, "invalid values for min and max, try something else!\n");
+		fprintf(stderr, "min: %d\n", ctx.min);
+		fprintf(stderr, "max: %d\n", ctx.max);
+	}
+	printf("hash: %s\nmin: %d\nmax: %d\n", ctx.hash, ctx.min, ctx.max);
+	do {
+		ctx.len++;
+		printf("trying %lu chars...\n", ctx.len);
+		val = brute_md5(ctx);
+	}while(val == NULL);
+	puts("solved!");
+	puts(val);
+
 	return 0;
 }
--- src/permutate.c
+++ src/permutate.c
@@ -2,39 +2,58 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <stdint.h>
 #include "permutate.h"
 #include "md5.h"
 
 static int inc_str(char *s, size_t len, char min, char max) {
 	size_t j = len;
 	size_t i;
+	for(i = 0; i < len && s[i] == max; i++) {}
+	if(i == len)
+		return 1;
 	if(s[len-1] == max) {
 		for(j = len-1; j > 0 && s[j] == max; j--)
 			s[j] = min;
 		s[j]++;
+		/*
+		if(j == 0 && s[j-1] == max) {
+			return 1;
+		} */
 	} else
 		s[len-1]++;
-	//printf("%s\n", s);
-	for(i = 0; i < len && s[i] == max; i++) { }
+	return 0;
 
-	return (i == len);
+}
+
+static int hashcmp(const char *x, const char *y) { /* NOTE: This works like strcmp, just that it returns true if one string terminates early*/
+	while((*x && *y)) {
+		if(*x != *y)
+			break;
+		x++;
+		y++;
+	}
+	return (*x && *y);
 }
 
 char *brute_md5(br_md5 ctx) {
-	if(ctx.min >= ctx.max) return (char *)-1; //error!
-
 	char *guess = malloc(ctx.len*sizeof(char));
+	int correct = 0;
 	for(size_t i = 0; i < ctx.len; i++) {
 		guess[i] = ctx.min;
 	}
 
 	do {
 		char *hash = compute_md5(guess, ctx.len);
-		if(!strcmp(hash, ctx.hash)) {
+		if(!hashcmp(ctx.hash, hash)) {
+			correct = 1;
 			break;
 		}
 		free(hash);
 	} while(!inc_str(guess, ctx.len, ctx.min, ctx.max));
-
-	return guess;
+	if(correct) return guess;
+	else {
+		free(guess);
+		return NULL;
+	}
 }
